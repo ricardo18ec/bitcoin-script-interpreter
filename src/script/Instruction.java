3d048d091ed2dcfe7c81f4b04e9fb43f58330231
MRMC07 <maremaza07@gmail.com>
2026-02-22 21:29:57 -0600

    Instruction

--- src/script/Instruction.java
+++ src/script/Instruction.java
@@ -1,5 +1,35 @@
 package script;
 
 public class Instruction {
-    
+
+    private Opcode opcode;
+    private byte[] data;
+
+    // Constructor para opcode
+    public Instruction(Opcode opcode) {
+        this.opcode = opcode;
+        this.data = null;
+    }
+
+    // Constructor para pushdata
+    public Instruction(byte[] data) {
+        this.data = data;
+        this.opcode = null;
+    }
+
+    public boolean isOpcode() {
+        return opcode != null;
+    }
+
+    public boolean isPushData() {
+        return data != null;
+    }
+
+    public Opcode getOpcode() {
+        return opcode;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
 }
